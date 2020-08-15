9a987bf5b7e4a5c5a275380535524b0f2837b268
AlejandraM31 <63984212+AlejandraM31@users.noreply.github.com>
2020-08-14 22:28:43 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,25 +1,17 @@
 const express = require('express');
-const morgan = require('morgan');
-const path = require('path');
-const app = express();
+const router = express.Router();
 
-// settings
-app.set('port', process.env.PORT || 3000);
-app.set('views', path.join(__dirname, 'views'));
-app.engine('html',require('ejs').renderFile);
-app.set('view engine', 'ejs');
-
-// middlewares
-app.use(morgan('dev'));
-
-// routes
-app.use(require('./routes/index'));
-
-//static files
-
-
-//listening the server
-
-app.listen(app.get('port'),()=>{
-    console.log('Server on port',app.get('port'));
+router.get('/', (req, res) => {
+  res.render('index.html', { title: 'Sitio Web Hotel Hesperia' });
 });
+
+router.get('/contact', (req, res) => {
+  res.render('contact.html', { title: 'Hoteles' });
+});
+router.get('/blog', (req, res) => {
+  res.render('blog.html', { title: 'Galeria' });
+});
+router.get('/zonaClientes', (req, res) => {
+  res.render('zonaClientes.html', { title: 'Clientes' });
+});
+module.exports = router;
